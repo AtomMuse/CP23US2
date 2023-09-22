12e8357365540ab24eda358bf401b5ad13fe302f
spytifo <supichaya18.work@gmail.com>
2023-09-22 12:43:50 +0700

    setup project

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+	<div class="w-screen h-screen flex justify-center items-center font-serif">
+		<div class="flex flex-col justify-center items-center gap-4">
+			<p class="text-4xl">Online Exhibition Management System</p>
+			<p>- coming soon -</p>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import 'animate.css'
+</script>
