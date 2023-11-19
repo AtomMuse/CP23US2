69a00b12caf6eb0d9a448f690a59ccf1e9ddf52c
sttrex-13 <sttrex.13.st@gmail.com>
2023-11-20 02:51:45 +0700

    Revert "feat: add Footer to reuse components🧩"
    This reverts commit e31770e223248792442b4813d4125f42f67845f5.

--- components/BaseFooter.vue
+++ components/BaseFooter.vue
@@ -1,27 +1,26 @@
-<script setup>
-import { ref } from 'vue'
-defineProps({
-  text: {
-    type: String,
-    require: true
-  }
-})
-</script>
+<script setup></script>
 
 <template>
   <footer
     class="fixed bottom-0 left-0 z-20 w-full p- bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-4 dark:bg-[#280E05] dark:border-gray-600"
   >
-    <div class="w-full max-w-screen-xl mx-auto p-2 md:p-2">
+    <div class="w-full max-w-screen-xl mx-auto p-4 md:py-4">
       <span
-        class="block text-gray-500 sm:text-center text-sm md:text-base dark:text-gray-300 "
+        class="block text-sm text-gray-500 sm:text-center dark:text-gray-300"
         style="display: block; text-align: center; margin: 0 auto; font-family: 'Inter', sans-serif;"
       >
-        {{text}}
+        © 2023 ATOMMUSE, SIT KMUTT.
       </span>
     </div>
   </footer>
 </template>
 
 <style scoped>
+@media (max-width: 768px) {
+  .footer .text-center {
+    display: block;
+    text-align: center;
+    margin: 0 auto;
+  }
+}
 </style>
