753e9a83e841c162f4da1f520d1e186bcc89ee36
spytifo <supichaya18.work@gmail.com>
2023-12-01 14:02:50 +0700

    add: exhibition list page

---
+++ pages/exhibitions/[id].vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="w-full h-full">
+		<div class="grid grid-cols-12 gap-5">
+			<BlogHeaderSection
+				:startDate="exhibition.startDate"
+				:endDate="exhibition.endDate"
+				:exhibitionName="exhibition.exhibitionName"
+				:exhibitionDescription="exhibition.exhibitionDescription"
+				class="col-start-3 col-span-8 py-16"
+			/>
+		</div>
+
+		<BlogSections
+			v-for="(section, index) in exhibition.exhibitiontionSections"
+			:key="index"
+			:sectionInfo="section"
+		/>
+	</div>
+	<div></div>
+</template>
+<script setup>
+const route = useRoute()
+const exhibition = ref([])
+const getExhibition = async () => {
+	const res = await fetch(`http://localhost:5000/exhibitions/${route.params.id}`)
+	exhibition.value = await res.json()
+}
+await getExhibition()
+</script>
+
+<style scoped></style>
---
+++ pages/exhibitions/index.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		exhibition list
+		<nuxt-link
+			:to="`/exhibitions/${exhibition.id}`"
+			v-for="(exhibition, index) in exhibitionsData"
+			:key="index"
+		>
+			<li>{{ exhibition.exhibitionName }}</li>
+		</nuxt-link>
+	</div>
+</template>
+
+<style scoped></style>
+
+<script setup>
+const exhibitionsData = ref([])
+const getExhibitions = async () => {
+	const res = await fetch('http://localhost:5000/exhibitions')
+	exhibitionsData.value = await res.json()
+}
+await getExhibitions()
+</script>
