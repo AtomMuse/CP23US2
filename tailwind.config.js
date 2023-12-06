a6d997d33faeee86f5c1cd836889393083e97aa5
spytifo <supichaya18.work@gmail.com>
2023-12-06 19:25:09 +0700

    add: set default color for tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [],
+	theme: {
+		extend: {
+			colors: {
+				darkBrown: '#280E05',
+				darkOrange: '#E0400B',
+				softOrange: '#F47340',
+				darkBlue: '#111827'
+			}
+		}
+	},
+	plugins: []
+}
